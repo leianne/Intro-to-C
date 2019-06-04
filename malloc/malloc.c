de30c811ab2d7b6fc28699c00b10c04ec34cc347
Leianne Louis <leilouis112@gmail.com>
2019-06-04 19:18:45 -0400

    updated malloc

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -12,7 +12,13 @@
 */
 char *string_dup(char *src)
 {
-
+    int length = strlen(src);
+    char *dub = malloc(length + 1);
+    for(int i = 0; i < length; i++){
+        *(dub + i) = *(src + i);
+    }
+    *(dub + length) = '\0';
+    return dub;
 }
 
 /*
@@ -24,6 +30,14 @@ char *string_dup(char *src)
 */
 void mem_copy(void *dest, const void *src, int n)
 {
+    char *s = src;
+    char *d = dest;
+
+    for (int i = 0; i < n; i++){
+        d[i] = s[i];
+    }
+
+    printf("%s", dest);
 
 }
 
@@ -43,7 +57,7 @@ void *resize_memory(void *ptr, int old_size, int new_size)
 
 }
 
-#ifndef TESTING
+// #ifndef TESTING
 int main(void)
 {
     char *s = "Some string to duplicate.";
@@ -65,23 +79,23 @@ int main(void)
 
     printf("\n");
 
-    char *url = string_dup("http://lambdaschool.com");
-    char *path = string_dup("/students/");
-    int url_length = string_length(url);
-    int path_length = string_length(path);
+    // char *url = string_dup("http://lambdaschool.com");
+    // char *path = string_dup("/students/");
+    // int url_length = string_length(url);
+    // int path_length = string_length(path);
     
-    int new_length = url_length - 1 + path_length;
-    char *new_url = resize_memory(url, url_length, new_length);
-    char *p = new_url + url_length;
+    // int new_length = url_length - 1 + path_length;
+    // char *new_url = resize_memory(url, url_length, new_length);
+    // char *p = new_url + url_length;
 
-    while (*path != '\0') {
-        *p = *path;
-        p++;
-        path++;
-    }
+    // while (*path != '\0') {
+    //     *p = *path;
+    //     p++;
+    //     path++;
+    // }
 
-    printf("Full path string: %s\n", new_url);
+    // printf("Full path string: %s\n", new_url);
 
     return 0;
 }
-#endif
+// #endif
